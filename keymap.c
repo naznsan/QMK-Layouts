38cd09fdb694198da5aa9a975828dac415cc564d
Justin Kim <naznsan@gmail.com>
2021-10-06 20:56:53 +1300

    Add Arrows layer

--- keymap.c
+++ keymap.c
@@ -25,13 +25,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,       KC_Y,       KC_U,                   KC_I,           KC_O,           KC_P,
     LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,       KC_H,       LSFT_T(KC_J),           LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_SCLN),
     KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,       KC_N,       KC_M,                   KC_COMM,        KC_DOT,         KC_SLSH,
-                                    LT(3, KC_DEL),         LT(1, KC_BSPC),        KC_TAB,     LT(6, KC_ENT),     LT(5, KC_SPC),          LT(7, KC_ESC)
+                                    LT(3, KC_DEL),         LT(1, KC_BSPC),        LT(2, KC_TAB),     LT(5, KC_ENT),     LT(4, KC_SPC),          LT(6, KC_ESC)
   ),
   // NAV
   [1] = LAYOUT_split_3x5_3(
     RESET,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_AGIN,   KC_PSTE,   KC_COPY,   KC_CUT,   KC_UNDO,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS,
     KC_NO,    KC_ALGR, KC_NO,    KC_NO,    KC_NO,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
+                      KC_NO,    KC_NO,    KC_NO,    KC_ENT,  KC_BSPC, KC_DEL
+  ),
+  // ARROWS
+  [2] = LAYOUT_split_3x5_3(
+    KC_NO,  KC_NO,   KC_UP,     KC_NO,    KC_NO,    KC_AGIN,   KC_PSTE,   KC_COPY,   KC_CUT,   KC_UNDO,
+    KC_NO,  KC_LEFT, KC_DOWN,   KC_RIGHT, KC_NO,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS,
+    KC_NO,  KC_ALGR, KC_NO,     KC_NO,    KC_NO,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
                       KC_NO,    KC_NO,    KC_NO,    KC_ENT,  KC_BSPC, KC_DEL
   ),
   // MEDIA
@@ -42,21 +49,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_NO,    KC_NO,    KC_NO,    KC_MSTP, KC_MPLY, KC_MUTE
   ),
   // NUM
-  [5] = LAYOUT_split_3x5_3(
+  [4] = LAYOUT_split_3x5_3(
     KC_LBRC,        KC_7,           KC_8,           KC_9,           KC_RBRC,    KC_NO,       KC_NO,           KC_NO,           KC_NO,          RESET,
     KC_QUOT,        KC_4,           KC_5,           KC_6,           KC_EQL,     KC_NO,       KC_LSFT,         KC_LCTL,         KC_RALT,        KC_LGUI,
     KC_GRV,         KC_1,           KC_2,           KC_3,           KC_BSLS,    KC_NO,       KC_NO,           KC_NO,           KC_ALGR,        KC_NO,
                                     KC_DOT,         KC_0,           KC_MINS,    KC_NO,       KC_NO,           KC_NO         
   ),
   // SYM
-  [6] = LAYOUT_split_3x5_3(
+  [5] = LAYOUT_split_3x5_3(
     KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, KC_NO,    KC_NO,    KC_NO,    KC_NO,    RESET,
     KC_DQUO, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS, KC_NO,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, KC_NO,    KC_NO,    KC_NO,    KC_ALGR, KC_NO,
     KC_LPRN, KC_RPRN, KC_UNDS, KC_NO,    KC_NO,   KC_NO 
   ),
   // FUN
-  [7] = LAYOUT_split_3x5_3(
+  [6] = LAYOUT_split_3x5_3(
     KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR, KC_NO,    KC_NO,    KC_NO,    KC_NO,    RESET,
     KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SLCK, KC_NO,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
     KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, KC_NO,    KC_NO,    KC_NO,    KC_ALGR, KC_NO,
@@ -74,10 +81,11 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 
 #define L_BASE 0
 #define L_NAV 1
+#define L_ARROWS 2
 #define L_MEDIA 3
-#define L_NUM 5
-#define L_SYM 6
-#define L_FUN 7
+#define L_NUM 4
+#define L_SYM 5
+#define L_FUN 6
 
 void oled_render_layer_state(void) {
     oled_write_P(PSTR("Layer: "), false);
@@ -88,8 +96,20 @@ void oled_render_layer_state(void) {
         case L_NAV:
             oled_write_ln_P(PSTR("Navigation\n"), false);
             break;
+        case L_ARROWS:
+            oled_write_ln_P(PSTR("Arrows\n"), false);
+            break;
         case L_MEDIA:
             oled_write_ln_P(PSTR("Media\n"), false);
+            break;
+        case L_NUM:
+            oled_write_ln_P(PSTR("Numbers\n"), false);
+            break;
+        case L_SYM:
+            oled_write_ln_P(PSTR("Symbols\n"), false);
+            break;
+        case L_FUN:
+            oled_write_ln_P(PSTR("Functions\n"), false);
             break;
     }
 }
